e7654158b80a958d74594319ce0cd900ce46af84
Youssou-Ndiaye <130571995+Youssou-Ndiaye@users.noreply.github.com>
2023-12-13 17:58:22 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/*function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}*/
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+//spinner
+const spinnerWrapper = document.querySelector(".spinner-wrapper");
+
+window.addEventListener("load", () => {
+  spinnerWrapper.style.opacity = "0";
+  setTimeout(() => {
+    spinnerWrapper.style.display = "none";
+  }, 200);
+});
